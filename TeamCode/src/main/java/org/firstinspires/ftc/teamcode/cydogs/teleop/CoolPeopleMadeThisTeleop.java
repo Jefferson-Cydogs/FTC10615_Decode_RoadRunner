9a21b2ca910c95bf5f8b8c83d68d00a4d7babb11
R20 <jefferson-cydogs@gmail.com>
2025-10-26 12:50:52 -0400

    General cleanup of TeleOp, including if statements for Driver/Manipulator controls Added ButtonSinglePressDetect

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cydogs/teleop/CoolPeopleMadeThisTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cydogs/teleop/CoolPeopleMadeThisTeleop.java
@@ -2,15 +2,12 @@ package org.firstinspires.ftc.teamcode.cydogs.teleop;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.VoltageSensor;
 
 import org.firstinspires.ftc.teamcode.cydogs.chassis.IndianaChassis;
-import org.firstinspires.ftc.teamcode.cydogs.components.AprilTagReader;
 import org.firstinspires.ftc.teamcode.cydogs.components.AprilTagReaderDuo;
 import org.firstinspires.ftc.teamcode.cydogs.components.ColorLED;
 import org.firstinspires.ftc.teamcode.cydogs.components.Feeders;
 import org.firstinspires.ftc.teamcode.cydogs.components.Intake;
-import org.firstinspires.ftc.teamcode.cydogs.components.Launchers;
 import org.firstinspires.ftc.teamcode.cydogs.components.LaunchersWithVelocity;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 
@@ -19,18 +16,16 @@ import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 public class CoolPeopleMadeThisTeleop extends LinearOpMode {
 
     // declare variables here
-
-    private LaunchersWithVelocity RocketLauncher3000;
-    private Feeders BumperCars;
+    private IndianaChassis Wheels;
     private Intake ArtifactEater;
+    private Feeders BumperCars;
+    private LaunchersWithVelocity RocketLauncher3000;
 
-    private IndianaChassis wheels;
     private ColorLED LauncherLED;
     private ColorLED RightLED;
     private ColorLED LeftLED;
 
     private AprilTagReaderDuo tagReader;
-
     private AprilTagDetection currentDetection;
 
     private double currentLauncherPower = 0.63;
@@ -40,133 +35,148 @@ public class CoolPeopleMadeThisTeleop extends LinearOpMode {
 
 
     @Override
-    public void runOpMode() {
+    public void runOpMode()
+    {
         double voltage;
+
         // Execute initialization actions here
-        wheels = new IndianaChassis(this);
-        wheels.InitializeTeleop(.7,.3,.5);
+        Wheels = new IndianaChassis(this);
+        Wheels.InitializeTeleop(.7,.3,.5);
 
         initializeDevices();
         initializePositions();
-        VoltageSensor voltageSensor = hardwareMap.voltageSensor.iterator().next();
+        //VoltageSensor voltageSensor = hardwareMap.voltageSensor.iterator().next();
 
         waitForStart();
-        while (opModeIsActive()) {
+        while (opModeIsActive())
+        {
             // Execute OpMode actions here
-            wheels.TraditionalTeleopDrive();
+            Wheels.TraditionalTeleopDrive();
             manageDriverControls();
             manageManipulatorControls();
 
-             voltage = voltageSensor.getVoltage();
-
-            telemetry.addData("Battery Voltage", voltage);
-            telemetry.addData("newPower:",currentLauncherPower);
-
-            if(RocketLauncher3000.IsMotorTooStrong(currentLauncherPower)){
-                LauncherLED.SetColor(.29);
+            if (RocketLauncher3000.IsMotorTooStrong(currentLauncherPower)) {
+                LauncherLED.SetColorByName("red");
+                //LauncherLED.SetColor(.29);
             }
-            else if(RocketLauncher3000.IsMotorAtSpeed(currentLauncherPower)){
-                LauncherLED.SetColor(1);
+            else if (RocketLauncher3000.IsMotorAtSpeed(currentLauncherPower)) {
+                LauncherLED.SetColorByName("white");
+                //LauncherLED.SetColor(1);
             }
             else {
-                LauncherLED.SetColor(0);
+                LauncherLED.SetColorByName("off");
+                //LauncherLED.SetColor(0);
             }
+
+            //voltage = voltageSensor.getVoltage();
+            //telemetry.addData("Battery Voltage", voltage);
+
+            telemetry.addData("LauncherPower:",currentLauncherPower);
             telemetry.update();
         }
     }
 
     private void manageDriverControls()
     {
-        if(gamepad1.y)
-        {
+        ButtonSinglePressDetect aButton = new ButtonSinglePressDetect();
+        ButtonSinglePressDetect bButton = new ButtonSinglePressDetect();
+        ButtonSinglePressDetect yButton = new ButtonSinglePressDetect();
+
+        if (yButton.isPressed(gamepad1.y)) {
             //currentDetection = tagReader.GetScoringTag("Red");
             currentLauncherPower += 0.05;
             RocketLauncher3000.RunAtVelocity(currentLauncherPower);
-            sleep(300);
-
+            //sleep(300);
         }
-        else if(gamepad1.a)
-        {
+        else if (aButton.isPressed(gamepad1.a)) {
             currentLauncherPower -= 0.05;
             RocketLauncher3000.RunAtVelocity(currentLauncherPower);
-            sleep(300);
+            //sleep(300);
         }
-        if(gamepad1.b)
-        {
-            tagReader.turnToFaceAprilTag(.4,5,wheels,"blue");
-
+        else if (bButton.isPressed(gamepad1.b)) {
+            tagReader.turnToFaceAprilTag(.4,5, Wheels,"blue");
         }
     }
-    private void manageManipulatorControls() {
+
+    private void manageManipulatorControls()
+    {
         if (gamepad2.a) {
             RocketLauncher3000.TurnPowerOff();
         }
-
-        if (gamepad2.y) {
+        else if (gamepad2.x) {
+            RocketLauncher3000.RunAtVelocity(-0.2);
+        }
+        else if (gamepad2.y) {
             RocketLauncher3000.RunAtVelocity(currentLauncherPower);
         }
+        else if (gamepad2.dpad_left) {
+            ArtifactEater.reverseleftintake();
+        }
+        else if (gamepad2.dpad_right) {
+            ArtifactEater.reverserightintake();
+        }
+
         if (gamepad2.right_bumper) {
             BumperCars.ActivateRightBumper();
             ArtifactEater.turnRightIntakeon();
         } else {
             BumperCars.DeactivateRightBumper();
             ArtifactEater.turnrightintakeoff();
-
-        }
-        if (gamepad2.x) {
-            RocketLauncher3000.RunAtVelocity(-0.2);
         }
 
-        if(gamepad2.left_bumper) {
+        if (gamepad2.left_bumper) {
             BumperCars.ActivateLeftBumper();
             ArtifactEater.turnLeftIntakeon();
         }
         else {
-             BumperCars.DeactivateLeftBumper();
-             ArtifactEater.turnleftintakeoff();
+            BumperCars.DeactivateLeftBumper();
+            ArtifactEater.turnleftintakeoff();
         }
 
-        if(gamepad2.left_trigger>.4) {
+        if (gamepad2.left_trigger > 0.4) {
             ArtifactEater.turnLeftIntakeon();
-        }
-        else if(gamepad2.dpad_left){
-            ArtifactEater.reverseleftintake();
         }
         else {
             ArtifactEater.turnleftintakeoff();
         }
 
-        if(gamepad2.dpad_right){
-            ArtifactEater.reverserightintake();
-        }
-        else if(gamepad2.right_trigger>.4){
+        if (gamepad2.right_trigger > 0.4) {
             ArtifactEater.turnRightIntakeon();
         }
         else {
             ArtifactEater.turnrightintakeoff();
         }
-
     }
-
-
 
     private void initializeDevices()
     {
-        RocketLauncher3000=new LaunchersWithVelocity(this);
-        ArtifactEater= new Intake(this);
-        BumperCars= new Feeders(this);
-        LauncherLED= new ColorLED(this,"LauncherLED");
+        RocketLauncher3000 = new LaunchersWithVelocity(this);
+        ArtifactEater = new Intake(this);
+        BumperCars = new Feeders(this);
+        LauncherLED = new ColorLED(this,"LauncherLED");
         RightLED = new ColorLED(this,"RightLED");
         LeftLED = new ColorLED(this,"LeftLED");
         tagReader = new AprilTagReaderDuo(this, "Red");
-
     }
 
     private void initializePositions()
     {
-        LauncherLED.SetColor(0);
-        RightLED.SetColor(.4);
-        LeftLED.SetColor(.7);
+        //LauncherLED.SetColor(0);
+        LauncherLED.SetColorByName("off");
+        //Testing RightLED and LeftLED
+        RightLED.SetColorByName("green");
+        LeftLED.SetColorByName("purple");
     }
 
+    public class ButtonSinglePressDetect
+    {
+        private boolean lastState = false;
+
+        public boolean isPressed(boolean currentState)
+        {
+            boolean result = currentState && !lastState;
+            lastState = currentState;
+            return result;
+        }
+    }
 }
