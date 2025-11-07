4c418f25048141cc3de302fce66709582e309e4f
JC <jeffersoncydogs@gmail.com>
2025-11-06 19:04:41 -0500

    Adding other auton corners

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cydogs/autonomous/RedNearPlus3V2.java
@@ -0,0 +1,154 @@
+package org.firstinspires.ftc.teamcode.cydogs.autonomous;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.teamcode.cydogs.chassis.IndianaAuton;
+import org.firstinspires.ftc.teamcode.cydogs.core.EventTracker;
+
+
+// The 'extends LinearOpMode' is needed so this code can run the build in op mode code from FIRST.
+//    @Autonomous puts this code in the autonomous category on driver station
+@Autonomous(name= "Red Near Plus 3 V2", group= "Autonomous", preselectTeleOp = "Cool People Blue")
+public class RedNearPlus3V2 extends LinearOpMode {
+
+
+    private double velocityPercentage = 0.41;
+
+    private IndianaAuton indiana;
+    private ElapsedTime currentTimer;
+    private EventTracker eventTracker;
+
+
+
+
+    @Override
+    public void runOpMode() {
+        // load 2 purple on left side
+        // load 1 green on right side by launcher
+
+
+         indiana = new IndianaAuton(this, "red");
+         indiana.InitializeAuton();
+        currentTimer = new ElapsedTime();
+        eventTracker = new EventTracker();
+        int startWaitTime = indiana.AskStartWaitTime();
+
+        // Wait for the start button to be pressed on the driver station
+        waitForStart();
+
+        if (opModeIsActive()) {
+            sleep(startWaitTime*1000);
+
+
+            // this clears bumper servo bug
+            indiana.Feeders.MoveBumpersToFixBug();
+
+            indiana.RedNearOpeningFlourish();
+
+    // #LauncherON
+            indiana.Launchers.RunAtVelocity(velocityPercentage);
+            indiana.RotateRight(60, 0.55, 200);
+
+
+            indiana.ShootThreeShots(velocityPercentage);
+// after shooting three, maybe eject what is left??
+            indiana.EjectAllArtifacts(2000);
+            //indiana.Launchers.TurnPowerOff();
+            indiana.Launchers.RunAtVelocity(0.2);
+            sleep(150);
+            //indiana.Launchers.RunAtVelocity(0.2);
+            //sleep(100);
+            indiana.Launchers.RunAtVelocity(0.09);
+            indiana.Launchers.StopLaunchersSafely();
+            //sleep(50);
+    // #LauncherOFF
+
+            indiana.RotateRight(29,.5,100);
+            indiana.StrafeRight(193, .45, 100);
+
+            // Get purple
+    // #Intake ON
+            indiana.Intake.turnIntakeOn();
+
+    // #LeftFeeder ON
+            indiana.Feeders.ActivateLeftBumper();
+            indiana.MoveStraight(425, .4, 100);
+            indiana.MoveStraight(320, .12, 300);
+
+    // #LeftFeeder OFF
+            indiana.Feeders.DeactivateLeftBumper();
+
+    // #LeftFeeder REVERSE
+            indiana.Feeders.ReverseLeftBumper();
+
+            // Get Green
+    // #RightFeeder ON
+            indiana.Feeders.ActivateRightBumper();
+            indiana.StrafeRight(127,.4, 100);
+            indiana.MoveStraight(270, .3, 200);
+
+    // #LeftFeeder OFF
+            indiana.Feeders.DeactivateLeftBumper();
+
+    // #RightFeeder OFF
+            indiana.Feeders.DeactivateRightBumper();
+
+
+            // Got green, back up
+            indiana.Launchers.ResetLaunchersToFloat();
+
+    // #Launchers REVERSE
+            indiana.Launchers.RunAtVelocity(-.15);
+            //indiana.Feeders.ReverseRightBumper();
+
+
+
+            indiana.MoveStraight(-970, .5, 100);
+
+
+    // #Intake OFF
+            indiana.Intake.turnIntakeOff();
+
+    // #Launchers OFF
+            indiana.Launchers.TurnPowerOff();
+            sleep(50);
+
+            // Go shoot
+    // #Launchers ON
+            double lastShotVelocity = 0.38;
+            indiana.Launchers.RunAtVelocity(lastShotVelocity);
+            //indiana.RotateRight(32, .5, 100);
+            indiana.StrafeLeft(1100, .6, 100);
+            indiana.RotateLeft(13,.5,100);
+            //indiana.MoveStraight(350, .8, 0);
+            //indiana.StrafeRight(400, .8, 200);
+
+            //indiana.TagReader.turnToFaceAprilTagAuton(indiana,indiana.Alliance, -8,.3, 3, currentTimer, eventTracker);
+            indiana.ShootThreeShotsSecondTime(lastShotVelocity);
+    // #Launchers OFF
+            indiana.Launchers.TurnPowerOff();
+
+            indiana.ColorLEDForAlliance();
+            sleep(2000);
+
+
+
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+}
