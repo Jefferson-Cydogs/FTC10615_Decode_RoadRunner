fb116eb643ac3628f2e8bb2ee98a16a25d6b58cc
R20 <jefferson-cydogs@gmail.com>
2025-11-08 02:09:26 -0500

    FV - Updated maximum distance for Artifacts

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cydogs/components/ColorFinder.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cydogs/components/ColorFinder.java
@@ -17,7 +17,7 @@ public class ColorFinder {
     //public static final float MIN_SATURATION = 0.5f;
     //public static final float MIN_VALUE = 0.3f;
     public static final int REQUIRED_CONSECUTIVE_HITS = 3;
-    final double DISTANCE_THRESHOLD_CM = 7.0;
+    final double DISTANCE_THRESHOLD_CM = 6.0;
     final int REQUIRED_MATCHES = 2;
     private long lastCheckTime = 0;
     private boolean lastResult = false;
@@ -68,7 +68,7 @@ public class ColorFinder {
 
     public boolean SeeColor2(TargetColor targetColor) {
         long currentTime = System.currentTimeMillis();
-        if (currentTime - lastCheckTime < 200) {
+        if (currentTime - lastCheckTime < 500) {
             return lastResult; // Return cached result
         }
 
