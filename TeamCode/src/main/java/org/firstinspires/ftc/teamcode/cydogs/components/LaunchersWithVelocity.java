07dc55ca118cd69b029ab47889224f4d3119c5ca
R20 <jefferson-cydogs@gmail.com>
2025-11-16 03:34:28 -0500

    FV - changed MaxTicksPerSecond to reflect motor with no gearbox Created VelocityTolerance variable for consistency Added setSafeVelocity method per goBILDA recommendation for Floodgate Power Switch Disconnect issue, but needs tuning

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cydogs/components/LaunchersWithVelocity.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cydogs/components/LaunchersWithVelocity.java
@@ -9,11 +9,12 @@ public class LaunchersWithVelocity
     // Specs for typical motors we use:
     //    GoBilda 5203 Series Yellow Jacket 223 RPM, 751.8 PPR
     //    GoBilda 5203 Series Yellow Jacket 312 RPM, 537.7 PPR
-    //       Without gearbox, theoretical 6,000RPM, empirical is 4,620RPM; ticks/second is always 28 at the motor shaft for goBilda Yellow Jacket motors
+    //       Without gearbox, theoretical 6,000RPM, empirical is 4,620RPM; ticks/second is always 28 at the motor shaft for goBILDA Yellow Jacket motors
     //    GoBilda 5203 Series Yellow Jacket 435 RPM, 384.5 PPR
     // Max TPS = (Motor's RPM / 60) * Motor's TicksPerRotation
-    public static final double MaxTicksPerSecond = (312.0 / 60.0) * 537.7; //TPS=2,796.04
-    //public static final double MaxTicksPerSecond = (4620.0 / 60.0) * 28.0; //TPS=2,156 without gearbox
+    //public static final double MaxTicksPerSecond = (312.0 / 60.0) * 537.7; //TPS=2,796.04
+    public static final double MaxTicksPerSecond = (4620.0 / 60.0) * 28.0; //TPS=2,156 without gearbox
+    private final double VelocityTolerance = 0.015; //1.5%
 
     private LinearOpMode opMode;
     public DcMotorEx Launchers;
@@ -30,7 +31,7 @@ public class LaunchersWithVelocity
         Launchers.setDirection(DcMotorEx.Direction.REVERSE);
         Launchers.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
         Launchers.setMode(DcMotorEx.RunMode.RUN_USING_ENCODER);
-        Launchers.setVelocityPIDFCoefficients(75, 0, 0, 15.4863);
+        Launchers.setVelocityPIDFCoefficients(75, 0, 0, 15.4863); //need to recalculate
     }
 
     public double GetCurrentVelocity() {
@@ -40,6 +41,18 @@ public class LaunchersWithVelocity
     public void RunAtVelocity(double TargetVelocityPercentage)
     {
         Launchers.setVelocity(TargetVelocityPercentage * MaxTicksPerSecond);
+    }
+
+    void setSafeVelocity(double targetVelocity)
+    {
+        final double SLEW_RATE = 0.2 * MaxTicksPerSecond;
+        double currentVelocity = Launchers.getVelocity();
+
+        double desiredChange = targetVelocity - currentVelocity;
+        double limitedChange = Math.max(-SLEW_RATE,
+                                        Math.min(desiredChange, SLEW_RATE));
+
+        Launchers.setVelocity(currentVelocity += limitedChange);
     }
 
     public void StopLaunchersSafely()
@@ -97,12 +110,12 @@ public class LaunchersWithVelocity
     {
         double TargetVelocity = TargetVelocityPercentage * MaxTicksPerSecond;
 
-        return (Math.abs(Launchers.getVelocity() - TargetVelocity) <= (TargetVelocity * 0.015));
+        return (Math.abs(Launchers.getVelocity() - TargetVelocity) <= (TargetVelocity * VelocityTolerance));
     }
 
     public boolean IsMotorTooStrong(double TargetVelocityPercentage)
     {
-        return (Launchers.getVelocity() > ((TargetVelocityPercentage * MaxTicksPerSecond) * 1.015));
+        return (Launchers.getVelocity() > ((TargetVelocityPercentage * MaxTicksPerSecond) * (1 + VelocityTolerance)));
     }
 
     public void TurnPowerOff() {
