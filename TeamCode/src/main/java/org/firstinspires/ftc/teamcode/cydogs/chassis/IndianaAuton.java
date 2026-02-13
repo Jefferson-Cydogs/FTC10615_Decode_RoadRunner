4310c1474ef1257abee5502f55f350801d41c367
JC <jeffersoncydogs@gmail.com>
2026-02-12 19:02:25 -0500

    Working on cleaning code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cydogs/chassis/IndianaAuton.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cydogs/chassis/IndianaAuton.java
@@ -16,7 +16,6 @@ public class IndianaAuton extends IndianaChassis {
     // declare devices
     public AprilTagReaderDuo TagReader;
     public IntakeV2 Intake;
-    public Intake Intakes;
     public Feeders Feeders;
     public LaunchersWithVelocity Launchers;
     public ColorLED LauncherLED;
