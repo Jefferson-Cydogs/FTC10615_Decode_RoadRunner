9d678598db484b370ca4c132c88c87e4f9e5b698
R20 <jefferson-cydogs@gmail.com>
2026-03-12 03:13:14 -0400

    FV - Removed unused method and associated variables

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cydogs/components/ArtifactSensors.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cydogs/components/ArtifactSensors.java
@@ -7,20 +7,12 @@ import org.firstinspires.ftc.teamcode.cydogs.core.TargetColor;
 
 
 public class ArtifactSensors {
-    public String LeftIntakeColor;
-    public String RightIntakeColor;
-    public String LeftLaunchColor;
-    public String RightLaunchColor;
     private ColorFinder LeftIntakeSensor;
     private ColorFinder RightIntakeSensor;
     private ColorFinder LeftLaunchSensor;
     private ColorFinder RightLaunchSensor;
     private TargetColor Green = TargetColor.ARTIFACTGREEN;
     private TargetColor Purple = TargetColor.ARTIFACTPURPLE;
-    private int ArtifactAtLeftIntake = 0;
-    private int ArtifactAtRightIntake = 0;
-    private int ArtifactAtLeftLaunch = 0;
-    private int ArtifactAtRightLaunch = 0;
     private ColorLED RightLED;
     private ColorLED LeftLED;
     private LinearOpMode myOpMode;
@@ -33,66 +25,16 @@ public class ArtifactSensors {
         LeftLaunchSensor = new ColorFinder(opMode, "LeftLaunchSensor");
         RightLaunchSensor = new ColorFinder(opMode,"RightLaunchSensor");
 
-        RightLED = new ColorLED(opMode,"RightLED");
         LeftLED = new ColorLED(opMode,"LeftLED");
-    }
-
-    public void CheckSensors() { CheckSensors(true); }
-    public void CheckSensors(boolean setLEDs)
-    {
-        if (LeftIntakeSensor.SeeArtifactColor(Green)) {
-            LeftIntakeColor = "Green";
-        }
-        else if (LeftIntakeSensor.SeeArtifactColor(Purple)) {
-            LeftIntakeColor = "Purple";
-        }
-        else {
-            LeftIntakeColor = "Nothing";
-        }
-        myOpMode.telemetry.addData("Left Intake Color:", LeftIntakeColor);
-
-        if (RightIntakeSensor.SeeArtifactColor(Green)) {
-            RightIntakeColor = "Green";
-        }
-        else if (RightIntakeSensor.SeeArtifactColor(Purple)) {
-            RightIntakeColor = "Purple";
-        }
-        else {
-            RightIntakeColor = "Nothing";
-        }
-        myOpMode.telemetry.addData("Right Intake Color:", RightIntakeColor);
-
-        if (LeftLaunchSensor.SeeArtifactColor(Green)) {
-            LeftLaunchColor = "Green";
-            if(setLEDs) {LeftLED.SetColorName(ColorOption.GREEN);}
-        }
-        else if (LeftLaunchSensor.SeeArtifactColor(Purple)) {
-            LeftLaunchColor = "Purple";
-            if(setLEDs) {LeftLED.SetColorName(ColorOption.PURPLE);}
-        }
-        else {
-            LeftLaunchColor = "Nothing";
-            if(setLEDs) {LeftLED.SetColorName(ColorOption.OFF);}
-        }
-        myOpMode.telemetry.addData("Left Launch Color:", LeftLaunchColor);
-
-        if (RightLaunchSensor.SeeArtifactColor(Green)) {
-            RightLaunchColor = "Green";
-            if(setLEDs) {RightLED.SetColorName(ColorOption.GREEN);}
-        }
-        else if (RightLaunchSensor.SeeArtifactColor(Purple)) {
-            RightLaunchColor = "Purple";
-            if(setLEDs) {RightLED.SetColorName(ColorOption.PURPLE);}
-        }
-        else {
-            RightLaunchColor = "Nothing";
-            if(setLEDs) {RightLED.SetColorName(ColorOption.OFF);}
-        }
-        myOpMode.telemetry.addData("Right Launch Color:", RightLaunchColor);
+        RightLED = new ColorLED(opMode,"RightLED");
     }
 
     public int CheckArtifactsColorAndCount(double MatchTimerInSeconds)
     {
+        int ArtifactAtLeftIntake;
+        int ArtifactAtRightIntake;
+        int ArtifactAtLeftLaunch;
+        int ArtifactAtRightLaunch;
         boolean UseLeftRightColorLEDs = (75 <= MatchTimerInSeconds);
 
         if (LeftIntakeSensor.SeeArtifactColor(Green) || LeftIntakeSensor.SeeArtifactColor(Purple)) {
@@ -110,12 +52,12 @@ public class ArtifactSensors {
         myOpMode.telemetry.addData("Artifact at Right Intake:", ArtifactAtRightIntake);
 
         if (LeftLaunchSensor.SeeArtifactColor(Green)) {
-            if (UseLeftRightColorLEDs) {
+            if (UseLeftRightColorLEDs) { //Reflect green Artifact color on LED only after 75s of TeleOp
                 LeftLED.SetColorName(ColorOption.GREEN);
             }
             ArtifactAtLeftLaunch = 1;
         } else if (LeftLaunchSensor.SeeArtifactColor(Purple)) {
-            if (UseLeftRightColorLEDs) {
+            if (UseLeftRightColorLEDs) { //Reflect purple Artifact color on LED only after 75s of TeleOp
                 LeftLED.SetColorName(ColorOption.PURPLE);
             }
             ArtifactAtLeftLaunch = 1;
@@ -128,12 +70,12 @@ public class ArtifactSensors {
         myOpMode.telemetry.addData("Artifact at Left Launch:", ArtifactAtLeftLaunch);
 
         if (RightLaunchSensor.SeeArtifactColor(Green)) {
-            if (UseLeftRightColorLEDs) {
+            if (UseLeftRightColorLEDs) { //Reflect green Artifact color on LED only after 75s of TeleOp
                 RightLED.SetColorName(ColorOption.GREEN);
             }
             ArtifactAtRightLaunch = 1;
         } else if (RightLaunchSensor.SeeArtifactColor(Purple)) {
-            if (UseLeftRightColorLEDs) {
+            if (UseLeftRightColorLEDs) { //Reflect purple Artifact color on LED only after 75s of TeleOp
                 RightLED.SetColorName(ColorOption.PURPLE);
             }
             ArtifactAtRightLaunch = 1;
