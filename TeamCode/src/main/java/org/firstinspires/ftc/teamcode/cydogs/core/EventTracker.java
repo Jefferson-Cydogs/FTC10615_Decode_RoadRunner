9495a4a988b53eb4b8a26036fb5ae9e0a243bdf1
R20 <jefferson-cydogs@gmail.com>
2026-03-12 02:00:32 -0400

    FV - Cleaned up code formatting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cydogs/core/EventTracker.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cydogs/core/EventTracker.java
@@ -7,41 +7,45 @@ public class EventTracker {
     private HashMap<String, Double> eventTimestamps;
 
     // Constructor
-    public EventTracker() {
+    public EventTracker()
+    {
         eventTimestamps = new HashMap<>();
     }
 
     public boolean doEvent(String eventName, double currentTime, double waitTime)
     {
-        if(currentTime > getLastTimestamp(eventName))
-        { //  allowed to do event
+        if (currentTime > getLastTimestamp(eventName)) { //Allowed to do event
             setTimestamp(eventName, currentTime + waitTime);
             return true;
         }
-        else
-        { // not allowed to do event
+        else { //Not allowed to do event
             return false;
         }
-
     }
     // Method to set the timestamp for a specific event
-    public void setTimestamp(String eventName, double timestamp) {
+    public void setTimestamp(String eventName, double timestamp)
+    {
         eventTimestamps.put(eventName, timestamp);
     }
 
     // Method to get the last timestamp of a specific event
-    public double getLastTimestamp(String eventName) {
+    public double getLastTimestamp(String eventName)
+    {
         double timeStamp = eventTimestamps.getOrDefault(eventName, 0.0);
         return timeStamp;
     }
 
     // Optional: Method to check if an event has been recorded
-    public boolean hasEvent(String eventName) {
+    public boolean hasEvent(String eventName)
+    {
         return eventTimestamps.containsKey(eventName);
     }
 
     // Optional: Method to clear all timestamps
-    public void clearTimestamps() {
+    public void clearTimestamps()
+    {
         eventTimestamps.clear();
     }
+
 }
+
