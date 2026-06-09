3a714692113795239b6541f72d92aab3dcb2d886
R20 <jefferson-cydogs@gmail.com>
2026-06-09 00:58:17 -0400

    FV - Disabled classes not used for competitions, including RR tuning ones and new AprilTagAlign

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cydogs/configs/MotorRPMReader.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cydogs/configs/MotorRPMReader.java
@@ -1,13 +1,14 @@
 package org.firstinspires.ftc.teamcode.cydogs.configs;
 
 import com.acmerobotics.dashboard.config.Config;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Config
-//@Disabled
+@Disabled
 @TeleOp
 public class MotorRPMReader extends LinearOpMode {
 
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cydogs/learning/AprilTagAlign.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cydogs/learning/AprilTagAlign.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.cydogs.learning;
 
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
@@ -21,6 +22,7 @@ import java.util.List;
  *
  */
 @TeleOp(name = "AprilTag Face Align", group = "Vision")
+@Disabled
 public class AprilTagAlign extends LinearOpMode {
 
     // ── Target tag ────────────────────────────────────────────────────────────
