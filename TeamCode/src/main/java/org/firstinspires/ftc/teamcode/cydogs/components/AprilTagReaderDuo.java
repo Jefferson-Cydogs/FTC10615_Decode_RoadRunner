b72dbe5adbde338e0d2bf3380036558a1ba2a1b9
R20 <jefferson-cydogs@gmail.com>
2026-06-11 00:47:54 -0400

    FV - Commented out resolution-related lines, while confirming best value; AprilTag detection was not working if forced to 1280x720 (webcam native)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cydogs/components/AprilTagReaderDuo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/cydogs/components/AprilTagReaderDuo.java
@@ -25,8 +25,8 @@ public class AprilTagReaderDuo {
     //private int leftMost=0;
     //private int rightMost=0;
     private static final boolean USE_WEBCAM = true;
-    private static final int RESOLUTION_WIDTH = 1280;
-    private static final int RESOLUTION_HEIGHT = 720;
+    //private static final int RESOLUTION_WIDTH = 1280;
+    //private static final int RESOLUTION_HEIGHT = 720;
     public int AprilTagId;
 
     public AprilTagDetection CurrentScoringTag;
@@ -53,10 +53,10 @@ public class AprilTagReaderDuo {
         // Set the camera (webcam vs. built-in RC phone camera).
         if (USE_WEBCAM) {
             builder.setCamera(opMode.hardwareMap.get(WebcamName.class, "Webcam 1"));
-            builder.setCameraResolution(new Size(RESOLUTION_WIDTH, RESOLUTION_HEIGHT));
+            //builder.setCameraResolution(new Size(RESOLUTION_WIDTH, RESOLUTION_HEIGHT));
         } else {
             builder.setCamera(BuiltinCameraDirection.BACK);
-            builder.setCameraResolution(new Size(RESOLUTION_WIDTH, RESOLUTION_HEIGHT));
+            //builder.setCameraResolution(new Size(RESOLUTION_WIDTH, RESOLUTION_HEIGHT));
         }
 
         // Build the Vision Portal, using the above settings.
